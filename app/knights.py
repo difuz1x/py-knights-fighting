3df5ae9428a523b954258471e28a6ecd01d26f78
difuz1x <klimenko1egor@gmail.com>
2026-04-29 03:57:27 +0300

    Solution after fix, added my own duel logic

--- app/knights.py
+++ app/knights.py
@@ -43,11 +43,11 @@ class Knight:
             self.potion_protection_effect = potion_obj.protection_effect
 
 # final stats
-        self.power = self.raw_power + self.weapon_power
-        + self.potion_power_effect
+        self.power = (self.raw_power + self.weapon_power
+        + self.potion_power_effect)
         self.hp = self.raw_hp + self.potion_hp_effect
-        self.protection = self.raw_protection + self.armour_protection
-        + self.potion_protection_effect
+        self.protection = (self.raw_protection + self.armour_protection
+        + self.potion_protection_effect)
         self.max_hp = self.hp
 
     def take_damage(self, opponent: Knight) -> None:
